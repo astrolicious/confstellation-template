185195b47bca79d545c89c259cc3631da8833c19
Alexander Niebuhr <alexander@nbhr.io>
2024-06-03 15:25:29 +0200

    Init template

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+import Confstellation from '@astrolicious/confstellation';
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+	output: 'server',
+	// adapter: ADD_YOUR_ADAPTER_HERE,
+	experimental: {
+		actions: true,
+	},
+	integrations: [
+		Confstellation({
+			config: {
+				conferenceName: 'Template',
+				conferenceSlogan: 'The best template conference',
+				conferenceDate: '2025-01-01T14:00:00Z',
+				nav: [
+					{ label: 'Home', link: '/' },
+				],
+			},
+		}),
+	],
+});
